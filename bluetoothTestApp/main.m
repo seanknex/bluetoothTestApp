5370aa75249e520348f9c30979635e89bd9de3dd
seanknex <seanbrown@Sean-Browns-MacBook-Pro.local>
2013-10-15 11:14:28 -0600

    Initial Commit

---
+++ bluetoothTestApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  bluetoothTestApp
+//
+//  Created by Sean Brown on 10/15/13.
+//  Copyright (c) 2013 Sound the Bell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "bluetoothTestAppAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([bluetoothTestAppAppDelegate class]));
+	}
+}
