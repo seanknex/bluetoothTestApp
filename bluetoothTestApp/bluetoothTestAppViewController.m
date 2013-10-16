84df14faacf6cccbc7bc68727fe3ea09a456e022
seanknex <seanbrown@null-d8a25e9889bc.app.byu.edu>
2013-10-16 09:53:25 -0600

    Initial Set Up

---
+++ bluetoothTestApp/bluetoothTestAppViewController.m
@@ -0,0 +1,29 @@
+//
+//  bluetoothTestAppViewController.m
+//  bluetoothTestApp
+//
+//  Created by Sean Brown on 10/15/13.
+//  Copyright (c) 2013 Sound the Bell. All rights reserved.
+//
+
+#import "bluetoothTestAppViewController.h"
+
+@interface bluetoothTestAppViewController ()
+
+@end
+
+@implementation bluetoothTestAppViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
